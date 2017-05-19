904b5c78b497c3c3651255287df7c7429e76e406
leemk <leemk@DESKTOP-A2KGL19>
2017-05-19 15:23:29 +0900

    mk

--- OOPS/src/oops/Main.java
+++ OOPS/src/oops/Main.java
@@ -1,93 +1,106 @@
 package oops;
 
-import java.awt.Component;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
-public class Main extends JFrame implements ActionListener {// OOPs first screen
+public class Main extends JFrame {
 
-	private JButton start_btn;// start button
-	private JButton introduce_btn;// introduce button
-	private JButton Exit_btn;// Exit button
-	private JButton Setting_btn;// Setting button
+	private JPanel contentPane;
+	private ImageIcon image;
 	private Font f1;
-	private JScrollPane scrollPane;
-	private OOPSTest ex;
+	final int frame_Height = 900;
+	final int frame_Width = 1300;
 
-	public Main() {
-
-		f1 = new Font("蹈框", Font.BOLD, 15);
-		start_btn = new JButton("START"); // start button 积己
-		start_btn.setFont(f1);
-
-		introduce_btn = new JButton("INTRODUCE");// introduce button 积己
-		introduce_btn.setFont(f1);
-
-		Setting_btn = new JButton("SETTING");// Setting button 积己
-		Setting_btn.setFont(f1);
-
-		
-		Exit_btn = new JButton("EXIT");// Exit button 积己
-		Exit_btn.setFont(f1);
-		
-		ImageIcon image = new ImageIcon("image.png");
-
-		JPanel panel= new JPanel() {
-			public void paintComponent(Graphics g){
-				g.drawImage(image.getImage(), 0, 0, null);
-				setOpaque(false);
-				super.paintComponent(g);
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main frame = new Main();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
-		};
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Main() {
+		setTitle("OOPS");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, frame_Width, frame_Height);
 		
-		panel.add(start_btn);
-		panel.add(introduce_btn);
-		panel.add(Setting_btn);
-		panel.add(Exit_btn);
+		image = new ImageIcon("image.png");
+		
+		contentPane = new JPanel(){ 
+	         public void paintComponent(Graphics g) {
+	             g.drawImage(image.getImage(), 0, 0, null);
+	             setOpaque(false);
+	             super.paintComponent(g);
+	          }
+	       };
+;
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		
+		 f1 = new Font("蹈框", Font.BOLD, 15);
 
 		
-		 scrollPane = new JScrollPane(panel);
-	     setContentPane(scrollPane);
-	
+		
+		setContentPane(contentPane);
+		
+		JButton btnStart = new JButton("Start");
+		btnStart.setFont(f1);
+		
+		JButton btnIntroduce = new JButton("Introduce");
+		btnIntroduce.setFont(f1);
+		
+		JButton btnSetting = new JButton("Setting");
+		btnSetting.setFont(f1);
+		
+		JButton btnExit = new JButton("Exit");
+		btnExit.setFont(f1);
+		
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+					.addGap(529)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(btnExit, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 236, Short.MAX_VALUE)
+						.addComponent(btnSetting, GroupLayout.DEFAULT_SIZE, 236, Short.MAX_VALUE)
+						.addComponent(btnIntroduce, GroupLayout.DEFAULT_SIZE, 236, Short.MAX_VALUE)
+						.addComponent(btnStart, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 236, Short.MAX_VALUE))
+					.addGap(507))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap(475, Short.MAX_VALUE)
+					.addComponent(btnStart, GroupLayout.PREFERRED_SIZE, 43, GroupLayout.PREFERRED_SIZE)
+					.addGap(35)
+					.addComponent(btnIntroduce, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+					.addGap(36)
+					.addComponent(btnSetting, GroupLayout.PREFERRED_SIZE, 44, GroupLayout.PREFERRED_SIZE)
+					.addGap(37)
+					.addComponent(btnExit, GroupLayout.PREFERRED_SIZE, 45, GroupLayout.PREFERRED_SIZE)
+					.addGap(88))
+		);
+		contentPane.setLayout(gl_contentPane);
 	}
-
-	public void start() {
-		start_btn.addActionListener(this);
-	}
-
-	public void introduce() {
-		introduce_btn.addActionListener(this);
-	}
-
-	public void setting() {
-		Setting_btn.addActionListener(this);
-	}
-
-	public void exit() {
-		Exit_btn.addActionListener(this);
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-
-//		if(arg0.getSource().equals(start_btn))
-//		{
-//			
-//		}
-//		else if(arg0.getSource().equals(Exit_btn))
-//			ex.change("Exit");
-//		else if(arg0.getSource().equals(Setting_btn))
-//			ex.change("Setting");
-//		else if(arg0.getSource().equals(introduce_btn))
-//			ex.change("Introduce");
-	}
-	
-	
-
 }

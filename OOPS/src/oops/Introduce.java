3264e4eda419641d2023adb3e9bb7bc664e7c038
leemk <leemk@DESKTOP-A2KGL19>
2017-05-23 00:04:16 +0900

    mk

--- OOPS/src/oops/Introduce.java
+++ OOPS/src/oops/Introduce.java
@@ -2,6 +2,7 @@ package oops;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
@@ -11,6 +12,14 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JLabel;
+import java.awt.Color;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
 
 public class Introduce extends JFrame {
 
@@ -18,6 +27,8 @@ public class Introduce extends JFrame {
 	private JPanel contentPane;
 	final int frame_Height = 900;
 	final int frame_Width = 1300;
+	final Font F_title;
+	final Font F_text;
 
 	/**
 	 * Launch the application.
@@ -35,24 +46,43 @@ public class Introduce extends JFrame {
 		
 		setBounds(100, 100, frame_Width, frame_Height);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
+		F_title=new Font("Times",Font.BOLD|Font.ITALIC,60);
+		F_text=new Font("Times",Font.BOLD,30);
+		JLabel lblIntroduce = new JLabel("How to Play");
+		lblIntroduce.setFont(F_title);
+		lblIntroduce.setForeground(new Color(255, 165, 0));
 		
-		JLabel lblIntroduce = new JLabel("Introduce");
+		JLabel lblNewLabel = new JLabel("1) In main screen, you click the start button.\r\n    ~~~~~");
+		lblNewLabel.setFont(F_text);
+		JLabel lblImage = new JLabel("image");
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(29)
-					.addComponent(lblIntroduce)
-					.addContainerGap(331, Short.MAX_VALUE))
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(79)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 823, GroupLayout.PREFERRED_SIZE)
+								.addComponent(lblIntroduce, GroupLayout.PREFERRED_SIZE, 498, GroupLayout.PREFERRED_SIZE)))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(521)
+							.addComponent(lblImage, GroupLayout.PREFERRED_SIZE, 167, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap(370, Short.MAX_VALUE))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(23)
-					.addComponent(lblIntroduce)
-					.addContainerGap(202, Short.MAX_VALUE))
+					.addGap(22)
+					.addComponent(lblIntroduce, GroupLayout.PREFERRED_SIZE, 150, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 113, GroupLayout.PREFERRED_SIZE)
+					.addGap(180)
+					.addComponent(lblImage, GroupLayout.PREFERRED_SIZE, 180, GroupLayout.PREFERRED_SIZE)
+					.addGap(191))
 		);
 		contentPane.setLayout(gl_contentPane);
 		
@@ -105,4 +135,6 @@ public class Introduce extends JFrame {
 		
 	}
 
+	
+	
 }

92e72ee962656280690862c3a556e5c16a3eb039
해선 <@192.168.0.112>
2017-05-22 19:31:34 +0900

    ...

--- OOPS/src/oops/End.java
+++ OOPS/src/oops/End.java
@@ -1,5 +1,103 @@
 package oops;
 
-public class End {
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-}
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.LayoutStyle.ComponentPlacement;
+
+public class End extends JFrame {
+
+   private JPanel contentPane;
+   private String id;
+   private int score;
+
+   /**
+    * Launch the application.
+    */
+   public static void main(String[] args) {
+      EventQueue.invokeLater(new Runnable() {
+         public void run() {
+            try {
+               End frame = new End();
+               frame.setVisible(true);
+            } catch (Exception e) {
+               e.printStackTrace();
+            }
+         }
+      });
+   }
+
+   /**
+    * Create the frame.
+    */
+   public End() {
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setBounds(100, 100, 450, 300);
+      contentPane = new JPanel();
+      contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+      setContentPane(contentPane);
+      
+      JPanel panel = new JPanel();
+      panel.setForeground(Color.BLUE);
+      panel.setBackground(Color.WHITE);
+      panel.setBorder(new TitledBorder(null, "End", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+      
+      JLabel lblOopsTheEnd = new JLabel("OOPS! THE END!");
+      lblOopsTheEnd.setFont(new Font("Berlin Sans FB Demi", Font.BOLD, 41));
+      
+      JLabel userScore = new JLabel("User: "+id);
+      userScore.setForeground(Color.BLUE);
+      userScore.setFont(new Font("Arial Black", Font.BOLD, 25));
+      JLabel userId = new JLabel("Score: "+score);
+      userId.setForeground(Color.RED);
+      userId.setFont(new Font("Arial Black", Font.PLAIN, 25));
+      GroupLayout gl_panel = new GroupLayout(panel);
+      gl_panel.setHorizontalGroup(
+         gl_panel.createParallelGroup(Alignment.LEADING)
+            .addGroup(gl_panel.createSequentialGroup()
+               .addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+                  .addGroup(gl_panel.createSequentialGroup()
+                     .addGap(57)
+                     .addComponent(lblOopsTheEnd))
+                  .addGroup(gl_panel.createSequentialGroup()
+                     .addGap(169)
+                     .addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
+                        .addComponent(userId)
+                        .addComponent(userScore))))
+               .addContainerGap(291, Short.MAX_VALUE))
+      );
+      gl_panel.setVerticalGroup(
+         gl_panel.createParallelGroup(Alignment.LEADING)
+            .addGroup(gl_panel.createSequentialGroup()
+               .addGap(32)
+               .addComponent(lblOopsTheEnd)
+               .addGap(42)
+               .addComponent(userScore)
+               .addPreferredGap(ComponentPlacement.RELATED)
+               .addComponent(userId)
+               .addContainerGap(16, Short.MAX_VALUE))
+      );
+      panel.setLayout(gl_panel);
+      GroupLayout gl_contentPane = new GroupLayout(contentPane);
+      gl_contentPane.setHorizontalGroup(
+         gl_contentPane.createParallelGroup(Alignment.LEADING)
+            .addGroup(gl_contentPane.createSequentialGroup()
+               .addComponent(panel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+               .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+      );
+      gl_contentPane.setVerticalGroup(
+         gl_contentPane.createParallelGroup(Alignment.LEADING)
+            .addComponent(panel, GroupLayout.DEFAULT_SIZE, 243, Short.MAX_VALUE)
+      );
+      contentPane.setLayout(gl_contentPane);
+   }
+
--- OOPS/src/oops/Join.java
+++ OOPS/src/oops/Join.java
@@ -1,5 +1,97 @@
 package oops;
 
-public class Join {
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.awt.event.ActionEvent;
+
+public class Join extends JFrame {
+
+   private JPanel contentPane;
+   private JTextField textField;
+   private ArrayList<String> userlist;
+   private String id;
+
+   /**
+    * Launch the application.
+    */
+   public static void main(String[] args) {
+      EventQueue.invokeLater(new Runnable() {
+         public void run() {
+            try {
+               Join frame = new Join();
+               frame.setVisible(true);
+            } catch (Exception e) {
+               e.printStackTrace();
+            }
+         }
+      });
+   }
+
+   /**
+    * Create the frame.
+    */
+   public Join() {
+      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      setBounds(100, 100, 450, 300);
+      contentPane = new JPanel();
+      contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+      contentPane.setLayout(new BorderLayout(0, 0));
+      setContentPane(contentPane);
+
+      JPanel panel = new JPanel();
+      panel.setBorder(new TitledBorder(null, "Log-in", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+      contentPane.add(panel, BorderLayout.CENTER);
+
+      JLabel lblId = new JLabel("ID: ");
+
+      JButton btnJoin = new JButton("Join");
+      JLabel login_warning = new JLabel();
+      JLabel join_warning = new JLabel();
+
+      textField = new JTextField();
+      textField.setColumns(10);
+
+      JButton btnLogin = new JButton("Log-in");
+      btnLogin.addActionListener(new ActionListener() {
+         public void actionPerformed(ActionEvent e) {
+            if (e.getSource().equals(btnLogin)) {
+               id=textField.getText();
+               if(userlist.contains(id)){
+                  //게임스타트로 전환해야함
+               }
+               else{
+                  login_warning.setText(id+"doesn't exist.\tPlease input existing id or create new id");
+               }
+            }
+            if(e.getSource().equals(btnJoin)){
+               id=textField.getText();
+               if(userlist.contains(id)){
+                  join_warning.setText(id+"exist already.\tPlease input new id");
+               }
+               else{
+                  userlist.add(id);
+               }
+                  
+            }
+         }
+      });
+
+   GroupLayout gl_panel = new GroupLayout(
+         panel);gl_panel.setHorizontalGroup(gl_panel.createParallelGroup(Alignment.LEADING).addGroup(gl_panel.createSequentialGroup().addGroup(gl_panel.createParallelGroup(Alignment.LEADING).addGroup(gl_panel.createSequentialGroup().addGap(60).addComponent(lblId).addPreferredGap(ComponentPlacement.RELATED).addComponent(textField,GroupLayout.PREFERRED_SIZE,GroupLayout.DEFAULT_SIZE,GroupLayout.PREFERRED_SIZE).addGap(14).addComponent(btnLogin)).addGroup(gl_panel.createSequentialGroup().addGap(164).addComponent(btnJoin))).addContainerGap(114,Short.MAX_VALUE)));gl_panel.setVerticalGroup(gl_panel.createParallelGroup(Alignment.LEADING).addGroup(gl_panel.createSequentialGroup().addGap(26).addGroup(gl_panel.createParallelGroup(Alignment.BASELINE).addComponent(lblId).addComponent(textField,GroupLayout.PREFERRED_SIZE,GroupLayout.DEFAULT_SIZE,GroupLayout.PREFERRED_SIZE).addComponent(btnLogin)).addGap(38).addComponent(btnJoin).addContainerGap(98,Short.MAX_VALUE)));panel.setLayout(gl_panel);
 }
+
+

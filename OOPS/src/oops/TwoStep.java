c22765282534142362e5bf225e36c2372cde7a58
leemk <leemk@DESKTOP-A2KGL19>
2017-05-31 23:06:55 +0900

    mk

--- OOPS/src/oops/TwoStep.java
+++ OOPS/src/oops/TwoStep.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
+import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
@@ -38,31 +39,31 @@ public class TwoStep extends JFrame {
 	private String answer;
 	private int lifenum;
 	private ImageIcon life;// life image
+	private ArrayList<JLabel> imageArray;
 
 	/**
 	 * Launch the application.
 	 */
-	public void setSound(boolean start) {
-		Sound = new BackSound("game2");
-		Sound.mulist(start);
+	public void setSound(boolean start,BackSound sound) {
+		this.Sound = sound;
+		
+		sound.mulist(start);
 	}
 
-	public void showlife(int num, JLabel label1, JLabel label2, JLabel label3) {
-		if (num == 3) {
-			label1.setVisible(true);
-			label2.setVisible(true);
-			label3.setVisible(true);
-		} else if (num == 2) {
-			label1.setVisible(true);
-			label2.setVisible(true);
-			label3.setVisible(false);
-		} else if (num == 1) {
-			label1.setVisible(true);
-			label2.setVisible(false);
-			label3.setVisible(false);
-		} else if (num <= 0) {
+	public void showlife(int num,ImageIcon life) {
+		
+		if(num<=0)
 			dispose();
+		else{
+			
+			for(int i=0;i<num;i++){
+				imageArray.get(i).setVisible(true);
+			}
+			for(int i=num;i<5-num;i++){
+				imageArray.get(i).setVisible(false);
+			}
 		}
+			
 	}
 
 	public static void main(String[] args) {
@@ -85,7 +86,8 @@ public class TwoStep extends JFrame {
 
 		setTitle("Two Step");
 		setBounds(100, 100, frame_Width, frame_Height);
-		setSound(true);
+		Sound = new BackSound("game2");
+		setSound(true,Sound);
 		caution = "Do you really want to close the window? If you close the window, you need to solve the problem again from the beginning.";
 		image = new ImageIcon("step2.png");
 
@@ -98,6 +100,15 @@ public class TwoStep extends JFrame {
 				super.paintComponent(g);
 			}
 		};
+		
+		
+		/*Life image Array*/
+		this.imageArray = new ArrayList<JLabel>();
+		for(int i=0;i<5;i++){
+			imageArray.add(new JLabel(life));
+		}
+		
+		
 
 		this.addWindowListener(new WindowListener() {
 
@@ -134,7 +145,7 @@ public class TwoStep extends JFrame {
 				} else if (window == JOptionPane.OK_OPTION) {
 
 					dispose();
-					setSound(false);
+					setSound(false,Sound);
 				}
 
 			}
@@ -142,7 +153,7 @@ public class TwoStep extends JFrame {
 			@Override
 			public void windowClosed(WindowEvent e) {
 				// TODO Auto-generated method stub
-				setSound(false);
+				setSound(false,Sound);
 				// main.setSound(true);
 				// main.setVisible(true);
 				dispose();
@@ -173,11 +184,7 @@ public class TwoStep extends JFrame {
 		panel_3.setVisible(false);
 		panel_4.setVisible(false);
 
-		JLabel lblNewLabel = new JLabel(life);
 
-		JLabel lblNewLabel_1 = new JLabel(life);
-
-		JLabel lblNewLabel_2 = new JLabel(life);
 
 		lifenum = 3;
 
@@ -195,7 +202,7 @@ public class TwoStep extends JFrame {
 				panel_2.setVisible(true);
 				panel_3.setVisible(false);
 				panel_4.setVisible(false);
-				showlife(lifenum, lblNewLabel, lblNewLabel_1, lblNewLabel_2);
+				showlife(lifenum,life);
 
 			}
 
@@ -215,7 +222,7 @@ public class TwoStep extends JFrame {
 				panel_2.setVisible(false);
 				panel_3.setVisible(true);
 				panel_4.setVisible(false);
-				showlife(lifenum, lblNewLabel, lblNewLabel_1, lblNewLabel_2);
+				showlife(lifenum,life);
 			}
 
 		});
@@ -233,7 +240,7 @@ public class TwoStep extends JFrame {
 				panel_2.setVisible(false);
 				panel_3.setVisible(false);
 				panel_4.setVisible(true);
-				showlife(lifenum, lblNewLabel, lblNewLabel_1, lblNewLabel_2);
+				showlife(lifenum,life);
 
 			}
 
@@ -252,50 +259,66 @@ public class TwoStep extends JFrame {
 				panel_2.setVisible(false);
 				panel_3.setVisible(false);
 				panel_4.setVisible(false);
-				showlife(lifenum, lblNewLabel, lblNewLabel_1, lblNewLabel_2);
+				showlife(lifenum,life);
 
 			}
 
 		});
+		
+		
 
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup().addGap(31).addGroup(gl_contentPane
-						.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup().addGap(10)
-								.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 225, GroupLayout.PREFERRED_SIZE)
-								.addGap(32)
-								.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 259,
-										GroupLayout.PREFERRED_SIZE)
-								.addGap(18).addComponent(lblNewLabel_2, GroupLayout.PREFERRED_SIZE, 256,
-										GroupLayout.PREFERRED_SIZE))
-						.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 900, GroupLayout.PREFERRED_SIZE)
-						.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 900, GroupLayout.PREFERRED_SIZE)
-						.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, 900, GroupLayout.PREFERRED_SIZE)
-						.addComponent(panel_4, GroupLayout.PREFERRED_SIZE, 900, GroupLayout.PREFERRED_SIZE))
-						.addContainerGap(341, Short.MAX_VALUE)));
-		gl_contentPane.setVerticalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup().addGap(29)
-						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 498, GroupLayout.PREFERRED_SIZE)
-								.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 498, GroupLayout.PREFERRED_SIZE)
-								.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, 498, GroupLayout.PREFERRED_SIZE)
-								.addComponent(panel_4, GroupLayout.PREFERRED_SIZE, 498,
-										GroupLayout.PREFERRED_SIZE))
-						.addGap(18)
-						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-								.addGroup(gl_contentPane.createSequentialGroup()
-										.addComponent(lblNewLabel, GroupLayout.PREFERRED_SIZE, 301,
-												GroupLayout.PREFERRED_SIZE)
-										.addGap(3))
-								.addGroup(Alignment.TRAILING,
-										gl_contentPane.createSequentialGroup()
-												.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-														.addComponent(lblNewLabel_2, Alignment.LEADING,
-																GroupLayout.DEFAULT_SIZE, 292, Short.MAX_VALUE)
-														.addComponent(lblNewLabel_1, GroupLayout.DEFAULT_SIZE, 292,
-																Short.MAX_VALUE))
-												.addContainerGap()))));
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(31)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+									.addGap(10)
+									.addComponent(imageArray.get(0), GroupLayout.PREFERRED_SIZE, 224, GroupLayout.PREFERRED_SIZE)
+									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+										.addGroup(gl_contentPane.createSequentialGroup()
+											.addGap(49)
+											.addComponent(imageArray.get(1), GroupLayout.PREFERRED_SIZE, 183, GroupLayout.PREFERRED_SIZE))
+										.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+											.addGap(95)
+											.addComponent(imageArray.get(3), GroupLayout.PREFERRED_SIZE, 137, GroupLayout.PREFERRED_SIZE))))
+								.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 900, GroupLayout.PREFERRED_SIZE)
+								.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 900, GroupLayout.PREFERRED_SIZE)
+								.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, 900, GroupLayout.PREFERRED_SIZE)
+								.addComponent(panel_4, GroupLayout.PREFERRED_SIZE, 900, GroupLayout.PREFERRED_SIZE)))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(535)
+							.addComponent(imageArray.get(2), GroupLayout.PREFERRED_SIZE, 179, GroupLayout.PREFERRED_SIZE)))
+					.addPreferredGap(ComponentPlacement.RELATED, 123, Short.MAX_VALUE)
+					.addComponent(imageArray.get(4), GroupLayout.PREFERRED_SIZE, 134, GroupLayout.PREFERRED_SIZE)
+					.addGap(84))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(29)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 498, GroupLayout.PREFERRED_SIZE)
+						.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 498, GroupLayout.PREFERRED_SIZE)
+						.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, 498, GroupLayout.PREFERRED_SIZE)
+						.addComponent(panel_4, GroupLayout.PREFERRED_SIZE, 498, GroupLayout.PREFERRED_SIZE))
+					.addGap(18)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+							.addComponent(imageArray.get(0), GroupLayout.PREFERRED_SIZE, 140, GroupLayout.PREFERRED_SIZE)
+							.addGap(3))
+						.addGroup(Alignment.TRAILING, gl_contentPane.createSequentialGroup()
+							.addComponent(imageArray.get(1), GroupLayout.PREFERRED_SIZE, 121, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED, 26, Short.MAX_VALUE)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
+								.addComponent(imageArray.get(3), GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+								.addComponent(imageArray.get(2), GroupLayout.DEFAULT_SIZE, 139, Short.MAX_VALUE)
+								.addComponent(imageArray.get(4), GroupLayout.PREFERRED_SIZE, 98, GroupLayout.PREFERRED_SIZE))
+							.addContainerGap())))
+		);
 		contentPane.setLayout(gl_contentPane);
 	}
 }

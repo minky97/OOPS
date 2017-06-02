ed725630c66ec923cdd6304cebe3d6928226cd9a
leemk <leemk@DESKTOP-A2KGL19>
2017-06-02 15:38:49 +0900

    mk

--- OOPS/src/oops/User.java
+++ OOPS/src/oops/User.java
@@ -4,24 +4,23 @@ import java.util.ArrayList;
 
 import oops.step.OneStep;
 
-public class User{
-   
-   private Main main = new Main();
-   private ArrayList<Integer> scorelist = new ArrayList<Integer>();
-    private Join join = new Join(main);
-    private Login login = new Login(main);
-    private OneStep onestep = new OneStep(main);
-    //private TwoStep twostep = new TwoStep(main);
-    //private ThreeStep threestep = new ThreeStep(main);
-    //private FourStep fourstep = new FourStep(main);
-    public int score(int index)
-    {   
-       int score_int = onestep.getScore();//+twostep.getScore()+threestep.getScore()+fourstep.getScore()
-       
-       scorelist.add(index, score_int);
-       
-       return scorelist.get(index);
-    }
+public class User {
 
-    
+	private Main main = new Main();
+	private ArrayList<Integer> scorelist = new ArrayList<Integer>();
+	private Join join = new Join(main);
+	private Login login = new Login(main);
+	private OneStep onestep = new OneStep(main);
+
+	// private TwoStep twostep = new TwoStep(main);
+	// private ThreeStep threestep = new ThreeStep(main);
+	// private FourStep fourstep = new FourStep(main);
+	public int score(int index) {
+		int score_int = onestep.getScore();// +twostep.getScore()+threestep.getScore()+fourstep.getScore()
+
+		scorelist.add(index, score_int);
+
+		return scorelist.get(index);
+	}
+
 }

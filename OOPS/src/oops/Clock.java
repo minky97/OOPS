2f9646e3e5b37c4e625aebe509b64ef72106ac45
USER <USER@DESKTOP-8FCKDPQ>
2017-06-09 13:25:28 +0900

    vv

--- OOPS/src/oops/Clock.java
+++ OOPS/src/oops/Clock.java
@@ -1,24 +1,51 @@
 package oops;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.Timer;
+//import javax.swing.Timer;
+
+import oops.step.OneStep;
+import oops.step.Step;
+
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 
-public class Clock extends JPanel implements ActionListener {
+public class Clock extends JPanel {
 
 	private ImageIcon icon = new ImageIcon("time.png");
 	
-	Timer mTimer = new Timer(1000,this);
-	int mTime = 3;
+	Timer timer = new Timer();
+	public void start()
+	{
+	//	timer.cancel();
+	TimerTask tt = new TimerTask() {
+		
+		@Override
+		public void run() {
+			
+			
+			if(time <= 0)
+			{
+				timer.cancel();
+			}
+		}
+	};
+	//timer = new Timer();
+	timer.schedule(tt, 1000);	
+	}
+	
+	int time;
 	
 	public Clock() {
 		
@@ -32,28 +59,33 @@ public class Clock extends JPanel implements ActionListener {
 				.addGap(0, 300, Short.MAX_VALUE)
 		);
 		setLayout(groupLayout);
-		mTimer.start();
+		
 	}
 
 	public void paintComponent(Graphics g)
 	{
-		g.drawString(""+mTime, 300, 300);
+		while(time>0)
+		{
+		g.setColor(Color.white);
+		g.fillRect(0, 0, 400, 400);
+		Font f = new Font("Times", Font.BOLD, 100);
+		g.setFont(f);
+		g.setColor(Color.black);
+		g.drawString(""+time, 230, 200);
+		time--;
+		}
 		g.drawImage(icon.getImage(), 0, 0, null);
     }
 
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		
-		if(mTimer == arg0.getSource())
-		{
-			mTime--;
-			if(mTime <= 0)
-			{
-			    mTimer.stop();
-				JOptionPane.showMessageDialog(this, "GameOver", "info", JOptionPane.INFORMATION_MESSAGE);
-			}
-			repaint();
-		}
+	
+	public int getTime() {
+		return time;
 	}
+
+	public void setTime(int time) {
+		this.time = time;
+	}
+
+	
 }
+
